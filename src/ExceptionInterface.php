34d3af6821efe7d021c97cb13468a3927fccd9dd
Andrii Vasyliev <sol@hiqdev.com>
2017-03-25 20:01:04 +0000

    added exceptions

---
+++ src/ExceptionInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace hiqdev\php\units\exceptions;
+
+/**
+ * Common interface for all exceptions thrown by this library.
+ *
+ * @author Andrii Vasyliev <sol@hiqdev.com>
+ */
+interface ExceptionInterface
+{
+}
---
+++ src/exceptions/NotConvertibleException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace hiqdev\php\units\exceptions;
+
+/**
+ * Thrown when cannot covert unit.
+ *
+ * @author Andrii Vasyliev <sol@hiqdev.com>
+ */
+final class NotConvertibleException extends \RuntimeException implements ExceptionInterface
+{
+}
